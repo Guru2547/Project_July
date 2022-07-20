22d428d4f1558168f10a29b35c4dd001ad87f804
Guru2547 <kururockers@gmail.com>
2022-07-20 11:07:16 +0530

    madhana code

--- src/test/java/org/test/sample/ProjectJuly.java
+++ src/test/java/org/test/sample/ProjectJuly.java
@@ -3,6 +3,9 @@ package org.test.sample;
 public class ProjectJuly {
 	public static void main(String[] args) {
 		
+		System.out.println(" Guru");
+		System.out.println(" saru");
+		System.out.println(" madhana");
 	}
 
 }
